162b215cd76257205d4cff285007eca753b44a61
Jordan Adler <jordan.m.adler@gmail.com>
2021-01-16 11:24:37 -0800

    Create dir-copying rig for test cases

--- spec/osproject_spec.rb
+++ spec/osproject_spec.rb
@@ -1,21 +1,29 @@
 require 'osproject'
+require 'tmpdir'
 
-RSpec.describe OSProject, "#initialize" do
-  context "with placeholder values" do
-    it "successfully instantitats the object" do
-      ios_proj = OSProject.new('ios', 'dir')
-      expect(ios_proj.type).to eq 'ios'
-      expect(ios_proj.dir).to eq 'dir'
+Dir.foreach("spec/samples") do |sampledir|
+  next if sampledir == '..' or sampledir == '.'
+  Dir.mktmpdir do |tmpdir| 
+    projdir = tmpdir + '/' + sampledir
+
+    RSpec.describe OSProject, "#initialize" do
+      context sampledir do
+        it "successfully instantitates the object" do
+          proj = OSProject.new('ios', projdir)
+          expect(proj.type).to eq 'ios'
+          expect(proj.dir).to eq projdir
+        end
+      end
+    end
+    RSpec.describe OSProject, ".add_sdk" do 
+      context "cocoapods + foobar" do
+        it "successfully adds sdk" do
+          ios_proj = OSProject.new('ios', 'dir')
+          ios_proj.add_sdk()
+          # XXX need to add a check here
+        end
+      end
     end
   end
 end
 
-RSpec.describe OSProject, ".add_sdk" do 
-  context "cocoapods + foobar" do
-    it "successfully adds sdk" do
-      ios_proj = OSProject.new('ios', 'dir')
-      ios_proj.add_sdk()
-      # XXX need to add a check here
-    end
-  end
-end
