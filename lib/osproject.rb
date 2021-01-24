52baf2058096f7cbb13fb1c049ebdb17af49b375
Jordan Adler <jordan.m.adler@gmail.com>
2021-01-23 16:57:52 -0800

    Stub out android add_sdk() function; Fix test-rig use of temp dirs

--- lib/osproject.rb
+++ lib/osproject.rb
@@ -15,6 +15,11 @@ class OSProject
     @os_app_id = os_app_id
   end
 
+  def _gsub_file(path, regexp, *args, &block)
+    content = File.read(path).gsub(regexp, *args, &block)
+    File.open(path, 'w') { |file| file.write(content) }
+  end
+
   # @abstract add_sdk is expected to be implemented by subclasses
   # @!method add_sdk
   #   Conduct the initial-setup process to add the latest OS SDK to the project
@@ -23,16 +28,54 @@ class OSProject
     puts "lang:" + self.lang + " os_app_id: " + self.os_app_id
     puts "apns_id:" + (self.apns_id || 'nil') + " fcm_id: " + (self.fcm_id || 'nil')
   end
-end
 
-class OSProject::GoogleAndroid < OSProject
-  def initialize(dir, lang, os_app_id)
-    super('android', dir, lang, os_app_id)
+  # @abstract has_sdk? is expected to be implemented by subclasses
+  # @!method has_sdk?
+  #   Returns True if the project already has an SDK initialized
+  def has_sdk?
+    raise Exception.new "Not Implemented"
   end
 end
 
 class OSProject::IOS < OSProject
+  # this is a temporary placeholder
+  attr_accessor :has_sdk
+
   def initialize(dir, lang, os_app_id)
+    @has_sdk = false
     super('ios', dir, lang, os_app_id)
+  end
+  def add_sdk
+    @has_sdk = true
+  end
+  def has_sdk?
+    return self.has_sdk
+  end
+end
+
+class OSProject::GoogleAndroid < OSProject
+  attr_accessor :app_class_location
+
+  @@kotlin_init_code = 'INIT'
+  @@java_init_code = 'INIT'
+
+  def initialize(dir, lang, os_app_id)
+    super('android', dir, lang, os_app_id)
+  end
+  
+  def add_sdk
+    # TODO: test for reqs
+    # add deps to /build.gradle
+    _gsub_file(dir + '/build.gradle', /(classpath 'com.android.tools.build:gradle:3.5.3')/, '\1')
+    # add deps to /app/build.gradle
+    _gsub_file(dir + '/app/build.gradle', /(implementation 'com.google.android.material:material:1.0.0')/, '\1ONESIGNAL\n')
+    # TODO: add init code to Application class
+    # _gsub_file(dir + self.app_class_location, //, '\1')
+    # TODO: prompt for custom notif icons & addl features
+  end
+
+  def has_sdk?
+    # TODO: more robust testing
+    return File.readlines(dir + '/app/build.gradle').grep(/ONESIGNAL/).any?
   end
 end
--- spec/osproject_spec.rb
+++ spec/osproject_spec.rb
@@ -11,28 +11,30 @@ Dir.foreach("spec/samples") do |sdk|
   sdk_class = sdkmap[sdk]
   Dir.foreach("spec/samples/" + sdk) do |sampledir|
     next if sampledir == '..' or sampledir == '.'
-    Dir.mktmpdir do |tmpdir| 
-      projdir = tmpdir + '/' + sampledir
-  
-      RSpec.describe OSProject, "#initialize" do
-        context sampledir do
-          it "successfully instantitates the object" do
-            proj = sdk_class.new(projdir, 'lang', 'app_id')
-            expect(proj.type).to eq sdk
-            expect(proj.dir).to eq projdir
-          end
-        end
+    tmpdir = Dir.mktmpdir()
+    projdir = tmpdir + '/' + sampledir
+    RSpec.describe OSProject, ".add_sdk" do
+      before(:all) do
+        puts 'sampledir: ', 'spec/samples/' + sdk + '/' + sampledir
+        puts 'projdir: ', projdir
+        FileUtils.cp_r('spec/samples/' + sdk + '/' + sampledir, projdir, verbose: true)
       end
-      RSpec.describe OSProject, ".add_sdk" do 
-        context "cocoapods + foobar" do
-          it "successfully adds sdk" do
-            proj = sdk_class.new(projdir, 'lang', 'app_id')
-            proj.add_sdk()
-            # XXX need to add a check here
-          end
+      after(:all) do
+        FileUtils.remove_entry tmpdir
+      end
+      context sampledir do
+        it "successfully instantitates the object" do
+          proj = sdk_class.new(projdir, 'lang', 'app_id')
+          expect(proj.type).to eq sdk
+          expect(proj.dir).to eq projdir
+        end
+        it "successfully adds sdk" do
+          proj = sdk_class.new(projdir, 'lang', 'app_id')
+          expect(proj.has_sdk?()).to eq false
+          proj.add_sdk()
+          expect(proj.has_sdk?()).to eq true
         end
       end
     end
   end
 end
-
