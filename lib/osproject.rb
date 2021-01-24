3ba4640c65dad24a0983ae76707a395bea65fbb4
Jordan Adler <jordan.m.adler@gmail.com>
2021-01-23 19:25:18 -0800

    Stashing code; Will erase this temp push when adjusting

--- lib/osproject.rb
+++ lib/osproject.rb
@@ -15,9 +15,25 @@ class OSProject
     @os_app_id = os_app_id
   end
 
-  def _gsub_file(path, regexp, *args, &block)
-    content = File.read(path).gsub(regexp, *args, &block)
+  def _gsub_file(path, match, replace, &block)
+    content = File.read(path).gsub(regexp, replace, &block)
     File.open(path, 'w') { |file| file.write(content) }
+  end
+
+  def _sub_file(path, match, replace, &block)
+    content = File.read(path).sub(regexp, replace, &block)
+    File.open(path, 'w') { |file| file.write(content) }
+  end
+
+  def _insert_lines(file, marker, insert)
+    str = Regexp::quote(marker)
+    if insert.is_a?
+      _sub_file(file, /^(\s*)(#{str})/) do |indent, prevln|
+        prevln + "\n" + indent + lines.join("\n" + indent)
+      end
+    else
+      _sub_file(file, /^(\s*)(#{str})/, '\1\2' + "\n" + '\1' + add)
+    end
   end
 
   # @abstract add_sdk is expected to be implemented by subclasses
@@ -56,26 +72,51 @@ end
 class OSProject::GoogleAndroid < OSProject
   attr_accessor :app_class_location
 
-  @@kotlin_init_code = 'INIT'
-  @@java_init_code = 'INIT'
-
   def initialize(dir, lang, os_app_id)
     super(:googleandroid, dir, lang, os_app_id)
   end
   
   def add_sdk!
     # TODO: test for reqs
+    #
+    # TODO: this gradle tack is a very brittle approach.
     # add deps to /build.gradle
-    _gsub_file(dir + '/build.gradle', /(classpath 'com.android.tools.build:gradle:3.5.3')/, '\1')
+    _insert_lines(dir + '/build.gradle', "jcenter()", "gradlePluginPortal()")
+    _insert_lines(dir + '/build.gradle', "classpath 'com.android.tools.build:gradle:3.5.3'", "classpath 'gradle.plugin.com.onesignal:onesignal-gradle-plugin:[0.12.9, 0.99.99]'")
     # add deps to /app/build.gradle
-    _gsub_file(dir + '/app/build.gradle', /(implementation 'com.google.android.material:material:1.0.0')/, '\1ONESIGNAL\n')
-    # TODO: add init code to Application class
-    # _gsub_file(dir + self.app_class_location, //, '\1')
-    # TODO: prompt for custom notif icons & addl features
+    _insert_lines(dir + '/app/build.gradle', "implementation 'com.google.android.material:material:1.0.0'", "implementation 'com.onesignal:OneSignal:[4.0.0, 4.99.99]'")
+    _insert_lines(dir + '/app/build.gradle', "apply plugin: 'com.android.application'", "plugins { id 'com.onesignal.androidsdk.onesignal-gradle-plugin' }")
+
+    # add OS API key to Application class
+    if self.lang == "java"
+      _sub_file(dir + '/' + self.app_class_location, /^(import \w*;)/, '\1' + "import com.onesignal.OneSignal;")
+      _sub_file(dir + '/' + self.app_class_location, /(\w+ extends Application {)/, '\1' + 'private static final String ONESIGNAL_APP_ID = "' + self.os_app_id + '";')
+      _insert_lines(dir + '/' + self.app_class_location, 'super.onCreate();', [
+        "// Enable verbose OneSignal logging to debug issues if needed.",
+        "// It is recommended you remove this after validating your implementation.",
+        "OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);",
+        "// OneSignal Initialization",
+        "OneSignal.initWithContext(this);",
+        "OneSignal.setAppId(ONESIGNAL_APP_ID);"
+      ])
+    elsif self.lang == "kotlin"
+      _sub_file(dir + '/' + self.app_class_location, /^(import \w*;)/, '\1' + 'import com.onesignal.OneSignal')
+      _sub_file(dir + '/' + self.app_class_location, /^(import \w*;)/, '\1' + 'const val ONESIGNAL_APP_ID = "' + self.os_app_id + '"')
+      _insert_lines(dir + '/' + self.app_class_location, 'super.onCreate()', [
+        "// Enable verbose OneSignal logging to debug issues if needed.",
+        "// It is recommended you remove this after validating your implementation.",
+        "OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)",
+        "// OneSignal Initialization",
+        "OneSignal.initWithContext(this)",
+        "OneSignal.setAppId(ONESIGNAL_APP_ID)"
+      ])
+    else 
+      raise "Don't know to handle #{lang}"
+    end
   end
 
   def has_sdk?
     # TODO: more robust testing
-    return File.readlines(dir + '/app/build.gradle').grep(/ONESIGNAL/).any?
+    return File.readlines(dir + '/app/build.gradle').grep(/OneSignal/).any?
   end
 end
--- spec/osproject_spec.rb
+++ spec/osproject_spec.rb
@@ -9,30 +9,39 @@ sdkmap = {
 Dir.foreach("spec/samples") do |sdk|
   next if sdk == '..' or sdk == '.'
   sdk_class = sdkmap[sdk]
-  Dir.foreach("spec/samples/" + sdk) do |sampledir|
-    next if sampledir == '..' or sampledir == '.'
-    tmpdir = Dir.mktmpdir()
-    projdir = tmpdir + '/' + sampledir
-    RSpec.describe OSProject, ".add_sdk" do
-      before(:all) do
-        puts 'sampledir: ', 'spec/samples/' + sdk + '/' + sampledir
-        puts 'projdir: ', projdir
-        FileUtils.cp_r('spec/samples/' + sdk + '/' + sampledir, projdir, verbose: true)
-      end
-      after(:all) do
-        FileUtils.remove_entry tmpdir
-      end
-      context sampledir do
-        it "successfully instantitates the object" do
-          proj = sdk_class.new(projdir, 'lang', 'app_id')
-          expect(proj.type.to_s).to eq sdk
-          expect(proj.dir).to eq projdir
+  Dir.foreach("spec/samples/" + sdk) do |lang|
+    next if lang == '..' or lang == '.'
+    Dir.foreach("spec/samples/" + sdk + '/' + lang) do |sampledirname|
+      next if sampledirname == '..' or sampledirname == '.'
+      sampledir = 'spec/samples/' + sdk + '/' + lang + '/' + sampledir
+      tmpdir = Dir.mktmpdir()
+      projdir = tmpdir + '/' + sampledir
+      RSpec.describe OSProject, ".add_sdk" do
+        before(:all) do
+          puts 'sampledir: ', sampledir
+          puts 'projdir: ', projdir
+          FileUtils.cp_r(sampledir, projdir, verbose: true)
         end
-        it "successfully adds sdk" do
-          proj = sdk_class.new(projdir, 'lang', 'app_id')
-          expect(proj.has_sdk?()).to eq false
-          proj.add_sdk!()
-          expect(proj.has_sdk?()).to eq true
+        after(:all) do
+          #FileUtils.remove_entry tmpdir
+        end
+        context sampledirname do
+          it "successfully instantitates the object" do
+            proj = sdk_class.new(projdir, lang, 'app_id')
+            expect(proj.type.to_s).to eq sdk
+            expect(proj.dir).to eq projdir
+          end
+          it "successfully adds sdk" do
+            proj = sdk_class.new(projdir, lang, 'app_id')
+            if sdk == :googleandroid
+              # For Android samples, we have a appclassfile symlink in the proj root dir
+              # When users use the code, they specify the file.
+              proj.app_class_location(appclassfile)
+            end
+            expect(proj.has_sdk?()).to eq false
+            proj.add_sdk!()
+            expect(proj.has_sdk?()).to eq true
+          end
         end
       end
     end
