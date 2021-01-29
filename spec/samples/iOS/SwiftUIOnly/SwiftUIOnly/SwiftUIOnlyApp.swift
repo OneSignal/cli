4d1c20c9c4205606ac9d9699f793847cef4b77c0
emawby <emawby@gmail.com>
2021-01-29 15:58:18 -0800

    Creating a fully SwiftUI sample project

---
+++ spec/samples/iOS/SwiftUIOnly/SwiftUIOnly/SwiftUIOnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOnlyApp.swift
+//  SwiftUIOnly
+//
+//  Created by Elliot Mawby on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOnlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
