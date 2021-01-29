ca8170cfb8fe7bec1f2949b9bfc45f2ddab6223a
emawby <emawby@gmail.com>
2021-01-29 15:55:46 -0800

    creating objc, storyboard, UIKit Sample Project

---
+++ spec/samples/iOS/ObjcExample/ObjcExample/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjcExample
+//
+//  Created by Elliot Mawby on 1/29/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
