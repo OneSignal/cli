c31506dbbd2c84b46cdf389a05201cba0aef098b
emawby <emawby@gmail.com>
2021-01-29 15:57:13 -0800

    creating a SwiftUI, App Delegate Sample Project

---
+++ spec/samples/iOS/SwiftUIAD/SwiftUIAD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAD
+//
+//  Created by Elliot Mawby on 1/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
