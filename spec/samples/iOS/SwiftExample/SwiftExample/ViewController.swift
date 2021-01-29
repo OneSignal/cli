7b36eb1f07c2c23187aa2f8be751c75e308ba186
emawby <emawby@gmail.com>
2021-01-29 15:54:18 -0800

    Creating Swift, storyboard, UIKit sample project

---
+++ spec/samples/iOS/SwiftExample/SwiftExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftExample
+//
+//  Created by Elliot Mawby on 1/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
