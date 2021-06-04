35235652d32d2639ab9f959413b4720e20bd4ae2
emawby <emawby@gmail.com>
2021-06-04 10:18:43 -0700

    adding help command and version option
    just running onesignal will now call the help command which will list out the available commands. Using OneSignal -h or --help will do the same thing.
    
    Using -v or --version will now print the version of the CLI

--- lib/oscli.rb
+++ lib/oscli.rb
@@ -4,10 +4,10 @@ require_relative 'osproject_ios'
 require_relative 'osproject_android'
 
 class AddCommand < Clamp::Command
-    option "--type", "TYPE", "project type (osx, android)", default: "osx"
-    parameter "PATH", "path to the project directory", default: "."
-    parameter "TARGETNAME", "Name of the target XCProject or appclassfile", default: ""
-    parameter "[LANG]", "language", default: "objc"
+    option [ "-t", "--type"], "TYPE", "project type (osx, android)"
+    parameter "PATH", "path to the project directory"
+    parameter "TARGETNAME", "Name of the target XCProject (osx) or appclassfile (android)"
+    parameter "LANG", "programming language to use"
     parameter "[APPID]", "OneSignal App ID", default: ""
 
     def execute  
@@ -18,11 +18,13 @@ class AddCommand < Clamp::Command
         'kotlin' => :kotlin
       }
       if type == 'osx'
-        ios_proj = OSProject::IOS.new(dir, targetname, langmap[lang], appid)
-        xcodeproj_path = dir + '/' + targetname + '.xcodeproj'
+        ios_proj = OSProject::IOS.new(path, targetname, langmap[lang], appid)
+        xcodeproj_path = path + '/' + targetname + '.xcodeproj'
         ios_proj.install_onesignal!(xcodeproj_path)
       elsif type == 'android'
-        OSProject::GoogleAndroid.new(dir, lang, appid).add_sdk!()
+        OSProject::GoogleAndroid.new(path, lang, appid).add_sdk!()
+      elsif !type
+        puts 'Please provide a project type (osx or android) with the --type option'
       else
         puts 'Invalid type (osx or android)'
       end
@@ -45,7 +47,31 @@ class MooCommand < Clamp::Command
     end
 end
 
+class HelpCommand < Clamp::Command
+  def execute
+    OSCLI.helptext
+  end
+end
+
 class OSCLI < Clamp::Command
-    subcommand "add_sdk", "Add the OneSignal SDK to the project", AddCommand,
+    option ["--version", "-v"], :flag, "Show version" do
+      puts "0.0.0"
+      exit(0)
+    end
+    option ["--help", "-h"], :flag, "Show Commands" do
+      OSCLI.helptext
+      exit(0)
+    end
+    self.default_subcommand = "help"
+    subcommand "help", "Lists the available commands in the OneSignal CLI", HelpCommand
+    subcommand "add_sdk", "Add the OneSignal SDK to the project", AddCommand
     subcommand "moo", "introduces you to a helpful cow", MooCommand
+
+    def self.helptext
+      puts <<~HELP 
+        add_sdk: Install the OneSignal SDK in the project
+        moo: Introduces you to a helpful cow
+        help: Lists the available commands in the OneSignal CLI
+      HELP
+    end
 end
