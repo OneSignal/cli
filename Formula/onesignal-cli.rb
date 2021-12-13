a0f7f02c50276a037865707774de107b55b96b93
Jeasmine Nahui <jeas.nahui@gmail.com>
2021-12-13 20:46:10 -0300

    Update Formula release version

--- Formula/onesignal-cli.rb
+++ Formula/onesignal-cli.rb
@@ -1,17 +1,23 @@
 class OnesignalCli < Formula
     desc "The OneSignal CLI is a tool to work with OneSignal projects."
     homepage "https://github.com/OneSignal/cli"
-    url "https://github.com/OneSignal/cli/archive/refs/tags/v0.0.1.tar.gz"
-    sha256 "32bf3183804b4c823669eabd426da0f5fb69f7130e8fae2baeba14665420e964"
+    url "https://github.com/OneSignal/cli/archive/refs/tags/0.0.2.tar.gz"
+    sha256 "8b711a13448c29c8a4a42b846684fb7d44fd751612323a0f8cb1fe36b6d4f5f0"
     license "MIT"
-    version "0.0.1"
+    version "0.0.2"
+
+    depends_on 'bash' => :run
 
     def install
+      prefix.install 'Gemfile'
+      tmpl.install Dir["tmpl/*"]
       lib.install Dir["lib/*"]
-      
       bin.install Dir["bin/*"]
     end
   
+    def postinstall
+      
+    end
     test do
       version_output = shell_output("#{bin}/onesignal version 2>&1")
       assert_match("cli  Version: #{version}", version_output)
