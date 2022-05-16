c1fc1ba7cd61e67690dbd43b1a64f3800349b31f
emawby <emawby@gmail.com>
2022-05-16 13:33:38 -0700

    Update formula to 0.0.4

--- Formula/onesignal-cli.rb
+++ Formula/onesignal-cli.rb
@@ -1,10 +1,10 @@
 class OnesignalCli < Formula
     desc "The OneSignal CLI is a tool to work with OneSignal projects."
     homepage "https://github.com/OneSignal/cli"
-    url "https://github.com/OneSignal/cli/archive/refs/tags/0.0.3.tar.gz"
-    sha256 "b74df2f09481f93f7e8240c2e348c653b4f5d2c8383d912b30e0cffb883b66d6"
+    url "https://github.com/OneSignal/cli/archive/refs/tags/0.0.4.tar.gz"
+    sha256 "a1f57cbab45a3322fd29a5dbbd3350fa30169c6338ec566af9c9b802dc45cef2"
     license "MIT"
-    version "0.0.3"
+    version "0.0.4"
 
     def install
       prefix.install 'Gemfile'
