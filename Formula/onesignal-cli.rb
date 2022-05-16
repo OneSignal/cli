091bb209004b7f534fdd3cc9f823572bf3485eb2
emawby <emawby@gmail.com>
2022-05-16 14:11:38 -0700

    fixing sha for 0.0.4 release

--- Formula/onesignal-cli.rb
+++ Formula/onesignal-cli.rb
@@ -2,7 +2,7 @@ class OnesignalCli < Formula
     desc "The OneSignal CLI is a tool to work with OneSignal projects."
     homepage "https://github.com/OneSignal/cli"
     url "https://github.com/OneSignal/cli/archive/refs/tags/0.0.4.tar.gz"
-    sha256 "a1f57cbab45a3322fd29a5dbbd3350fa30169c6338ec566af9c9b802dc45cef2"
+    sha256 "b1c4a0ec7a4c5781c823578cd75302db61fcc10e6f46c88805777bcc84323a94"
     license "MIT"
     version "0.0.4"
 
