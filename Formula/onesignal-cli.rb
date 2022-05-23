bd8ab2eba4902362434ee9932af73ea2a8baded4
emawby <emawby@gmail.com>
2022-05-23 10:15:51 -0700

    Updating onesignal-cli.rb to use the gemspec
    The url and sha will be updated to a release version once a new release is made with the proper tarball.
    
    Updating sha

--- Formula/onesignal-cli.rb
+++ Formula/onesignal-cli.rb
@@ -1,23 +1,31 @@
 class OnesignalCli < Formula
-    desc "The OneSignal CLI is a tool to work with OneSignal projects."
-    homepage "https://github.com/OneSignal/cli"
-    url "https://github.com/OneSignal/cli/archive/refs/tags/0.0.4.tar.gz"
-    sha256 "0ebfc2ce74af73421828bc03e5c7f11367c42903b2123655a7ec2ace004db3d4"
-    license "MIT"
-    version "0.0.4"
+  desc "The OneSignal CLI is a tool to work with OneSignal projects."
+  homepage "https://github.com/OneSignal/cli"
+  url "https://github.com/OneSignal/cli/archive/refs/tags/gemspec-test.tar.gz"
+  sha256 "25d0354a2c4237034eaa736c8ccdefb324b39dd4620e08f257c059273e5487e2"
+  license "MIT"
+  version "gemspec-test"
 
-    def install
-      prefix.install 'Gemfile'
-      include.install Dir["include/*"]
-      lib.install Dir["lib/*"]
-      bin.install Dir["bin/*"]
+  depends_on "ruby" if Hardware::CPU.arm?
+  uses_from_macos "ruby", since: :catalina
+
+  def install
+    if MacOS.version >= :mojave && MacOS::CLT.installed?
+      ENV["SDKROOT"] = ENV["HOMEBREW_SDKROOT"] = MacOS::CLT.sdk_path(MacOS.version)
     end
-  
-    def postinstall
-      
-    end
-    test do
-      version_output = shell_output("#{bin}/onesignal version 2>&1")
-      assert_match("cli  Version: #{version}", version_output)
-    end
+
+    ENV["GEM_HOME"] = libexec
+
+    system "gem", "build", "onesignal-cli.gemspec"
+    system "gem", "install", "onesignal-cli-1.0.0.gem"
+
+    bin.install libexec/"bin/onesignal"
+    # onesignal executable is now run from the libexec/bin folder
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
+
+  test do
+    version_output = shell_output("#{bin}/onesignal version 2>&1")
+    assert_match("cli  Version: #{version}", version_output)
+  end
+end
