487b83ea4150afa1d52b7c5bc77714b4fe72f277
emawby <emawby@gmail.com>
2022-05-23 15:29:16 -0700

    updating the python script and action to update the formula

--- .github/workflows/update_formula.py
+++ .github/workflows/update_formula.py
@@ -1,25 +1,14 @@
 import sys
 
-def update_version(version):
+def update_version(version, revision):
   formula = open('Formula/onesignal-cli.rb', 'r')
   lines = formula.readlines()
-  lines[3] = "  url \"https://github.com/OneSignal/cli/archive/refs/tags/{}.tar.gz\"\n".format(version)
-  lines[6] = "  version \"{}\"\n".format(version)
-
-  formula = open('Formula/onesignal-cli.rb', 'w')
-  formula.writelines(lines)
-  formula.close()
-
-def update_checksum(checksum):
-  formula = open('Formula/onesignal-cli.rb', 'r')
-  lines = formula.readlines()
-  lines[4] = "  sha256 \"{}\"\n".format(checksum)
+  lines[3] = "  url \"https://github.com/OneSignal/cli.git\", tag: \"{}\", revision: \"{}\"\n".format(version, revision)
 
   formula = open('Formula/onesignal-cli.rb', 'w')
   formula.writelines(lines)
   formula.close()
 
 newReleaseVersion = str(sys.argv[1])
-newChecksum = str(sys.argv[2])
-update_version(newReleaseVersion)
-update_checksum(newChecksum)
+newRevision = str(sys.argv[2])
+update_version(newReleaseVersion, newRevision)
