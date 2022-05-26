ce29fca371c9fb998651f3abe9937d1f65affa59
emawby <runner@fv-az318-357.ewvrrcgabbbubjc05i3n4a3pph.dx.internal.cloudapp.net>
2022-05-26 17:25:40 +0000

    Updating the onesignal-cli formula for the latest release

--- Formula/onesignal-cli.rb
+++ Formula/onesignal-cli.rb
@@ -1,7 +1,7 @@
 class OnesignalCli < Formula
   desc "The OneSignal CLI is a tool to work with OneSignal projects."
   homepage "https://github.com/OneSignal/cli"
-  url "https://github.com/OneSignal/cli.git", tag: "gemspec-test", revision: "e39a9c2d4c02633f6a23465ad0037ff98e180653"
+  url "https://github.com/OneSignal/cli.git", tag: "0.0.5", revision: "b615f99ea27e911202af492ca7aedfcbe8847102"
   license "MIT"
   version "gemspec-test"
 
